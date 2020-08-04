e0eea5114f2454e111c6d8afe0a42e1c95752173
Jonas Diego <jonasdiegorodriguesmartins@yahoo.com.br>
2020-08-03 23:45:44 -0300

    Application start

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+    response.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (request, response) => {
+    response.render('room', { roomId: request.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+let port = process.env.PORT || 3000
+server.listen(port, () => console.log(`Escutando na porta ${port}`))
